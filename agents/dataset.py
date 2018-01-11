Add time cost display; fix restricted-softmax problem.

agents/dataset.py
@@ -48,7 +48,7 @@ class SCReplayDataset(Dataset):
         data = self._load_data(self._filelist[file_id], frame_id)
         obs_screen, obs_minimap, action_available = self._transform_observation(
             data["observation"])
-        policy_label = self._transform_actions(data["actions"])
+        policy_label = self._transform_actions(data["actions"], action_available)
         value_label = 1 if data["result"] == 1 else 0
         sample = {'screen_feature': obs_screen,
                   'minimap_feature': obs_minimap,
@@ -68,24 +68,29 @@ class SCReplayDataset(Dataset):
                                  if filename.endswith(".tar")]
         self._total_num_frames = sum(self._num_frames_list)
 
-    def _transform_actions(self, actions):
+    def _transform_actions(self, actions, action_available):
         label = np.zeros(self._action_total_dim, dtype=np.float32)
         if len(actions) > 0:
             action = actions[0]
-            label[action.function] = 1
-            for arg_id, arg_val in zip(self._action_args_map[action.function],
-                                       action.arguments):
-                if len(arg_val) == 1:
-                    arg_val = arg_val[0]
-                    assert arg_val < self._action_head_dims[arg_id + 1]
-                    label[self._action_args_offset[arg_id] + arg_val] = 1
-                elif len(arg_val) == 2:
-                    arg_val = arg_val[1] * self._resolution + arg_val[0]
-                    assert arg_val < self._action_head_dims[arg_id + 1]
-                    label[self._action_args_offset[arg_id] + arg_val] = 1
-                else:
-                    raise NotImplementedError
-        else:
+        has_valid_action = False
+        for action in actions:
+            if action_available[action.function] == 0: # 0 valid, 1e30 invalid
+                label[action.function] = 1
+                for arg_id, arg_val in zip(self._action_args_map[action.function],
+                                           action.arguments):
+                    if len(arg_val) == 1:
+                        arg_val = arg_val[0]
+                        assert arg_val < self._action_head_dims[arg_id + 1]
+                        label[self._action_args_offset[arg_id] + arg_val] = 1
+                    elif len(arg_val) == 2:
+                        arg_val = arg_val[1] * self._resolution + arg_val[0]
+                        assert arg_val < self._action_head_dims[arg_id + 1]
+                        label[self._action_args_offset[arg_id] + arg_val] = 1
+                    else:
+                        raise NotImplementedError
+                has_valid_action = True
+                break
+        if not has_valid_action:
             label[0] = 1
         return label
 
@@ -123,8 +128,8 @@ class SCReplayDataset(Dataset):
         obs_minimap = self._transform_spatial_features(
             observation["minimap"], MINIMAP_FEATURES)
         num_actions =  self._action_head_dims[0]
-        action_available = np.zeros(num_actions, dtype=np.float32)
-        action_available[observation["available_actions"]] = 1
+        action_available = np.ones(num_actions, dtype=np.float32) * 1e30
+        action_available[observation["available_actions"]] = 0
         assert obs_screen.shape[1] == self._resolution
         assert obs_screen.shape[2] == self._resolution
         assert obs_minimap.shape[1] == self._resolution

agents/sl_agent.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import print_function
 
 import os
+import time
 import numpy as np
 
 import torch
@@ -66,6 +67,7 @@ class SLAgent(object):
                                     pin_memory=self._use_gpu,
                                     num_workers=num_dataloader_worker)
         num_batches, num_epochs, total_loss = 0, 0, 0
+        last_time = time.time()
         while num_epochs < max_epochs:
             for batch in dataloader_train:
                 screen_feature = batch["screen_feature"]
@@ -98,7 +100,10 @@ class SLAgent(object):
                 num_batches += 1
                 if num_batches % print_freq == 0:
                     print("Training Epochs: %d  Batches: %d Avg Train Loss: %f"
-                          % (num_epochs, num_batches, total_loss / print_freq))
+                          " Speed: %.2f s/batch"
+                          % (num_epochs, num_batches, total_loss / print_freq,
+                             (time.time() - last_time) / print_freq))
+                    last_time = time.time()
                     total_loss = 0
                 if num_batches % save_model_freq == 0:
                     valid_loss = self.evaluate(dataloader_dev)
@@ -227,7 +232,7 @@ class FullyConvNet(nn.Module):
 
         value_logit = self.value_fc(state)
         first_dim = self._action_dims[0] 
-        policy_logit = torch.cat([nonspatial_policy[:, :first_dim] * mask,
+        policy_logit = torch.cat([nonspatial_policy[:, :first_dim] - mask,
                                   spatial_policy,
                                   nonspatial_policy[:, first_dim:]],
                                  dim=1)