0eb2f0b9f077030ae7dc576f675c86e6e300dccf
Xinghai Sun <sunxinghai1216@gmail.com>
2018-01-17 11:10:34 +0800

    Update gen_unittype_whitelist for txt_of_gzip format.

--- utils/gen_unittype_whitelist.py
+++ utils/gen_unittype_whitelist.py
@@ -3,19 +3,16 @@ import sys
 import random
 import cPickle as pickle
 import StringIO
-import tarfile
+import base64
 import gzip
 from collections import Counter
 
 
 def count_unittype(filepath):
-    """Format tar_of_gzip supports fast random access."""
-    with tarfile.open(filepath, 'r') as tar:
-        all_contents = []
+    with open(filepath, 'r') as f:
         counter = Counter()
-        for tarinfo in tar.getmembers():
-            f = tar.extractfile(tarinfo)
-            content = f.read()
+        for line in f:
+            content = base64.b64decode(line)
             gfile = gzip.GzipFile(fileobj=StringIO.StringIO(content))
             frame = pickle.loads(gfile.read())
             counter += Counter(frame["observation"]["screen"][6].flatten())
@@ -26,7 +23,7 @@ root_dir = sys.argv[1]
 num_sampled_files = int(sys.argv[2])
 counter = Counter()
 for filename in random.sample(os.listdir(root_dir), num_sampled_files):
-    if filename.endswith(".tar"):
+    if filename.startswith("part"):
         filepath = os.path.join(root_dir, filename)
         counter += count_unittype(filepath)
         print(len(counter))
